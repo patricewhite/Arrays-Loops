6272908fb03e0c6b1f6d3a9a49b662013e256d80
Jamie Wang <sephona@gmail.com>
2017-05-09 14:07:27 -0400

    Fixed FizzBuzz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,19 +1,15 @@
-//3 fizz
-//5 buzz
-//3&5 fizzbuzz
-
 function fizzBuzz(countTO){
   let arr = [];
-  for(let i = 1; i <= countTO; i++){
-    switch(true){
-      case (i%15 === 0):
-        arr.push("fizzbuzz");
+  for(let i = 1; i <= countTO; i++) {
+    switch(i % 15) {
+      case 3: case 6: case 9: case 12:
+        arr.push("fizz");
         break;
-      case (i%5 === 0):
+      case 5: case 10:
         arr.push("buzz");
         break;
-      case (i%3 === 0):
-        arr.push("fizz");
+      case 0:
+        arr.push("fizzbuzz");
         break;
       default:
         arr.push(i);
